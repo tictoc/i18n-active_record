aa578be2497b45c12c9d8dc5397637be106ea8b7
Alan <alan@tictocfamily.com>
2013-03-05 08:41:52 +0000

    i18n plural keys doesn't exist We don't want to store active record translations in db

--- lib/i18n/backend/active_record/missing.rb
+++ lib/i18n/backend/active_record/missing.rb
@@ -42,8 +42,8 @@ module I18n
 
           unless ActiveRecord::Translation.locale(locale).lookup(key).exists?
             interpolations = options.keys - I18n::RESERVED_KEYS
-            keys = count ? I18n.t('i18n.plural.keys', :locale => locale).map { |k| [key, k].join(FLATTEN_SEPARATOR) } : [key]
-            keys.each { |key| store_default_translation(locale, key, interpolations) }
+            keys = [key]
+            keys.each { |key| store_default_translation(locale, key, interpolations) unless key =~ /activerecord/ }
           end
         end
 
